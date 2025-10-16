a7db518a7604fe1a668bdb020881c0e91e43d4c0
Yanxiang Meng <meng.yanxiang@outlook.com>
2025-10-16 22:59:32 +0200

    Fixed package bug

--- pkgbuild.R
+++ pkgbuild.R
@@ -3,3 +3,8 @@ library("pkgbuild")
 
 dest <- dirname(getwd())
 build(getwd(), dest_path = dest)
+
+
+# if(!require(shinyFiles)) {
+#   install.packages("../shinyFiles_0.9.3.9000.tar.gz", repos = NULL, type = "source")
+# }
