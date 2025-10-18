8fa722d1a305a567c38654f25716246d3c52e4b5
Yanxiang Meng <meng.yanxiang@outlook.com>
2025-10-18 19:22:54 +0200

    updated version number and build into package

---
+++ pkgbuild.R
@@ -0,0 +1,14 @@
+# build this repo into a package
+library("pkgbuild")
+
+dest <- dirname(getwd())
+build(getwd(), dest_path = dest)
+
+# rename the package to shinyFiles_MultiDirChoose_0.9.3.9001.tar.gz
+file.rename(
+    file.path(dest, "shinyFiles_0.9.3.9001.tar.gz"), 
+    file.path(dest, "shinyFiles_MultiDirChoose_0.9.3.9001.tar.gz")
+    )
+# if(!require(shinyFiles)) {
+#   install.packages("../shinyFiles_0.9.3.9000.tar.gz", repos = NULL, type = "source")
+# }
